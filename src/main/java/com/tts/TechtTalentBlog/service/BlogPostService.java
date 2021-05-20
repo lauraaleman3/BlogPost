58997cf1a9163207607db84ea2553bb268ef9461
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-05-20 09:04:08 -0500

    first commit

---
+++ src/main/java/com/tts/TechtTalentBlog/service/BlogPostService.java
@@ -0,0 +1,13 @@
+package com.tts.TechtTalentBlog.service;
+
+import com.tts.TechtTalentBlog.model.BlogPost;
+
+import java.util.Optional;
+
+// this service is going to serve as a contract
+// for our implementation
+public interface BlogPostService {
+
+    BlogPost addNewBlogPost(BlogPost blogPost);
+
+}
