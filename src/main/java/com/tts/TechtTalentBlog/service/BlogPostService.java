306feb783f7414e58fab49a09a3a698da6e9294d
Laura Aleman <79146743+lauraaleman3@users.noreply.github.com>
2021-05-21 09:43:11 -0500

    Updated blog

--- src/main/java/com/tts/TechtTalentBlog/service/BlogPostService.java
+++ src/main/java/com/tts/TechtTalentBlog/service/BlogPostService.java
@@ -2,6 +2,7 @@ package com.tts.TechtTalentBlog.service;
 
 import com.tts.TechtTalentBlog.model.BlogPost;
 
+import java.util.List;
 import java.util.Optional;
 
 // this service is going to serve as a contract
@@ -9,5 +10,11 @@ import java.util.Optional;
 public interface BlogPostService {
 
     BlogPost addNewBlogPost(BlogPost blogPost);
+
+    Iterable<BlogPost> getAllBlogPosts();
+
+    void deletePostById(Long id);
+
+//    List<BlogPost> getAllBlogPostsasList();
 
 }
